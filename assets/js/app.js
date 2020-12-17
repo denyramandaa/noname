5a65e6357ab654ce156f1ef4a964f8d68f414b9c
Deny Ramanda <deny@kompas.com>
2020-12-17 15:11:11 +0700

    dada

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,47 @@
-/*global Vue*/
-Vue.config.devtools = true;
-'use strict';
-new Vue({
-  el: '#app',
-  data:{
-    jumbotronImage: ['01.jpg','hd2.jpg','hd3.jpg','hd4.jpg'],
-    menus: [{
-      name: 'about us',
-      url: 'about-us'
-    },{
-      name: 'our product',
-      url: 'our-product'
-    },{
-      name: 'why us?',
-      url: 'why-us'
-    },{
-      name: 'gallery',
-      url: 'our-gallery'
-    },{
-      name: 'articles',
-      url: 'articles'
-    },{
-      name: 'contact us',
-      url: 'contact-us'
-    }],
-    openMenu: false,
-    scrolled: false
-  },
-  methods:{
-    initSwiper(){
-      var _self = this;
-      swiper = new Swiper('.swiper--jumbotron', {
-          effect: 'fade',
-          followFinger: false,
-          centeredSlides: true,
-          loop: true,
-          slidesPerView: 1,
-          spaceBetween: 0,
-          speed: 1000,
-          navigation: {
-            nextEl: '.jumbotron--next',
-            prevEl: '.jumbotron--prev',
-          },
-          pagination: {
-            el: '.jumbotron--pagination',
-            clickable: true
-          },
-          autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          }
-      });
-    },
-    registScroll: function (){
-      window.scrollY  > document.getElementsByClassName('jumbotron')[0].clientHeight/2 ? this.scrolled = true : this.scrolled = false
-    },
-    gointTo(a) {
-      window.scroll({
-        behavior: 'smooth',
-        left: 0,
-        top: document.getElementById(a).offsetTop - 50
-      })
-    }
-  },
-  mounted(){
-    this.$nextTick(function(){
-      this.initSwiper();
+$(document).ready(function() {
+  function initSwiper(){
+    new Swiper('.swiper--jumbotron', {
+        effect: 'fade',
+        followFinger: false,
+        centeredSlides: true,
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        speed: 1000,
+        navigation: {
+          nextEl: '.jumbotron--next',
+          prevEl: '.jumbotron--prev',
+        },
+        pagination: {
+          el: '.jumbotron--pagination',
+          clickable: true
+        },
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        }
     });
-  },
-  created(){
-    window.addEventListener('scroll', this.registScroll);
-  },
-  beforeDestroy(){
-    window.removeEventListener('scroll', this.registScroll);
   }
+  initSwiper();
+  $(window).scroll(function() {
+    window.scrollY  > document.getElementsByClassName('jumbotron')[0].clientHeight/2 ? $('header.header').addClass('active') : $('header.header').removeClass('active')
+  });
+  $(window).load(function(){
+    // loader
+  });
 });
+function gointTo(a) {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: document.getElementById(a).offsetTop - 50
+  })
+}
+function openMenu() {
+  $('.side-menu').addClass('show');
+  $('.overlay-side').fadeIn(100);
+}
+function closeMenu() {
+  $('.side-menu').removeClass('show');
+  $('.overlay-side').fadeOut(100);
+}
